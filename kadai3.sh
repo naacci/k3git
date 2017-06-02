fa44dd10482ba4876038745882c893bef28cf3a2
okumac <okumac@gmail.com>
2017-06-02 19:46:30 +0900

    kadai3 upload

---
+++ kadai3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+a=$1
+b=$2
+c=`expr $a \* $b`
+echo "2つの積は"$c"です"
+while [ $a -ne $b ]; do
+ if [ $a -gt $b ]; then
+  a=`expr $a - $b`
+ elif [ $a -lt $b ]; then
+  b=`expr $b - $a`
+ fi
+done
+echo "最大公約数は"$a"="$b
+echo "最小公倍数は"`expr $c / $a`
