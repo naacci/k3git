9c132a155f6d9933ba213dc621fb04a7dceef006
okumac <okumac@gmail.com>
2017-06-09 19:54:50 +0900

    numbers only

--- kadai3.sh
+++ kadai3.sh
@@ -2,6 +2,12 @@
 
 a=$1
 b=$2
+
+expr $a + $b > /dev/null 2>&1
+if [ $? -eq 2 ]; then
+ echo "only natural numbers allowed"
+else
+
 c=`expr $a \* $b`
 echo "2つの積は"$c"です"
 while [ $a -ne $b ]; do
@@ -13,3 +19,5 @@ while [ $a -ne $b ]; do
 done
 echo "最大公約数は"$a
 echo "最小公倍数は"`expr $c / $a`
+echo `expr $1 \* $2`
+fi
